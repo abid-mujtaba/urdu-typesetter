4351043a5e26d5a218d183ba7d72485c5d0430d6
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2022-08-28 12:37:46 -0400

    Move assets and pdf template into image and copy into build folder

--- toolchain/src/urdu_builder/build/pdf.py
+++ toolchain/src/urdu_builder/build/pdf.py
@@ -1,6 +1,27 @@
 """Build the pdf artifact."""
 
+from pathlib import Path
+from shutil import copy
+
 
 def build(source: str) -> None:
     """Build the pdf artifact."""
-    print(f"Building pdf with source: {source}")
+    build_dir = Path("/") / "build" / "pdf"
+
+    pre_populate_build(build_dir)
+    inject_template(build_dir)
+
+
+def pre_populate_build(build_dir: Path):
+    """Create a build folder and copy over assets."""
+    if build_dir.exists():  # Clean up if present
+        build_dir.rmdir()
+
+    build_dir.mkdir()
+    assets = Path("/") / "assets"
+
+    copy(assets / "fonts" / "jameel-noori-nastaleeq.ttf", build_dir)
+
+
+def inject_template(build_dir: Path):
+    """Inject source data into template."""
