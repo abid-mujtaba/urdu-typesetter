c944ce41d551693bf83280fe12cfa5d1a7cd3cdd
Abid H. Mujtaba <abid.naqvi83@gmail.com>
2022-08-28 09:08:34 -0400

    Implement arg parsing and routing to urdu_builder

--- toolchain/src/urdu_builder/__main__.py
+++ toolchain/src/urdu_builder/__main__.py
@@ -1,11 +1,61 @@
 """Entrypoint for module executed as script."""
 
-import sys
+
+from argparse import ArgumentParser
+
+
+def parse_args():
+    """Parse CLI args."""
+    parser = ArgumentParser(
+        description="Build/Preview Urdu literature artifacts from yaml source"
+    )
+
+    subparsers = parser.add_subparsers()
+
+    preview = subparsers.add_parser("preview")
+    preview.set_defaults(func=preview_func)
+    preview.add_argument(
+        "source_dir",
+        nargs="?",
+        default=".",
+        type=str,
+        help="Folder containing the yaml source files",
+    )
+
+    build = subparsers.add_parser("build")
+    build.set_defaults(func=build_func)
+    build.add_argument(
+        "--format",
+        type=str,
+        choices=("all", "epub", "html", "pdf"),
+        default="all",
+        help="Type of artifact to generate",
+    )
+    build.add_argument(
+        "source_dir",
+        nargs="?",
+        default=".",
+        type=str,
+        help="Folder containing the yaml source files",
+    )
+
+    kwargs = vars(parser.parse_args())
+
+    func = kwargs.pop("func")
+    func(**kwargs)
+
+
+def build_func(format: str, source_dir: str):
+    print(f"build {format} {source_dir}")
+
+
+def preview_func(source_dir: str):
+    print(f"preview {source_dir}")
 
 
 def main():
     """Entrypoint of script."""
-    print(f"Executing urdu_builder with args: {sys.argv}")
+    parse_args()
 
 
 if __name__ == "__main__":
